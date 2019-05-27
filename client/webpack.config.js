be43c55336f1c0be8c70395ca4a3f1c6ae0898b1
Fernando de Lima <fernando.dpaula@gmail.com>
2019-05-27 13:54:20 -0300

    instalando bootstrap como módulo

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,7 +5,7 @@ const babiliPlugin = require('babili-webpack-plugin');
 let plugins = [];
 
 //process acessa todas as variaveis de ambiente do node
-if(process.env.NODE_ENV == 'production'){
+if (process.env.NODE_ENV == 'production') {
     plugins.push(new babiliPlugin());
 }
 
@@ -20,7 +20,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         //definindo o caminho publico do bundle como 'dist/bundle.js'
         publicPath: 'dist'
-    }, 
+    },
 
     module: {
         rules: [
@@ -31,10 +31,29 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            }, {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+            },
+            {
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
             }
         ]
     },
 
     //declarando que plugins recebe o array plugins (plugins = plugins)
-    plugins 
+    plugins
 }
